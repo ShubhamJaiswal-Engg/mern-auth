7064c8240426407b2aebf3054deded972e080ad0
Shubham Jaiswal <shubhamjai8662@gmail.com>
2025-12-28 00:47:17 +0530

    setup authentication

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,3 +1,9 @@
 import express from "express";
-const appRoute = express.Router();
+import { login, logout, register } from "../controllers/authController.js";
+const authRouter = express.Router();
 
+authRouter.post('/register', register);
+authRouter.post('/login', login);
+authRouter.post('/logout', logout);
+
+export default authRouter;
--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,21 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongoose.js";
+import authRouter from "./routes/authRouter.js";
 const app = express();
 
 connectDB();
 
 const port = process.env.PORT||4000;
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({credentials:true}));
+
+app.get("/",(req,res)=>{
+    res.send("API Working");
+});
+app.use("/api/auth",authRouter);
 
 app.listen(port,()=>{
     console.log(`Server start listening at ${port}`);
